f29f639793803921a65462faf5fc0c8788868759
NukalaGayathrinetha <136892828+NukalaGayathrinetha@users.noreply.github.com>
2025-10-21 19:10:42 +0530

    Update sqlQueries.sql

--- scripts/silver/sqlQueries.sql
+++ scripts/silver/sqlQueries.sql
@@ -1,4 +1,4 @@
--- fix crm_cust_info
+
 
 
 INSERT INTO silver.crm_cust_info (
@@ -10,51 +10,54 @@ INSERT INTO silver.crm_cust_info (
 			cst_gndr,
 			cst_create_date
 		)
-select 
-cst_id,
-cst_key,
-TRIM(cst_firstname) as cst_firstname,
-TRIM(cst_lastname) as cst_lastname,
-CASE
- WHEN Upper(TRIM(cst_marital_status)) = 'S' then 'Single'
- WHEN Upper(TRIM(cst_marital_status)) = 'M' then 'Married'
- else 'n/a'
- end as cst_marital_status,
- CASE
- WHEN Upper(TRIM(cst_gndr)) = 'M' then 'Male'
- WHEN Upper(TRIM(cst_gndr)) = 'F' then 'Female'
- else 'n/a'
- end as cst_gndr,
- cst_create_date
-from
-(
-SELECT *,
-ROW_NUMBER() OVER(PARTITION by cst_id ORDER BY cst_create_date DESC) as rank_by_create_date
-from bronze.crm_cust_info
-) as t
-where rank_by_create_date = 1 and cst_id is not null;
+		SELECT
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE 
+				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_marital_status, -- Normalize marital status values to readable format
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
+			cst_create_date
+		FROM (
+			SELECT
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+			FROM bronze.crm_cust_info
+			WHERE cst_id IS NOT NULL
+		) t
+		WHERE flag_last = 1;
 
 
--- fix crm_prd_info
 
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_prd_info;
-GO
-
-CREATE TABLE silver.crm_prd_info (
-    prd_id          INT,
-    cat_id          NVARCHAR(50),
-    prd_key         NVARCHAR(50),
-    prd_nm          NVARCHAR(50),
-    prd_cost        INT,
-    prd_line        NVARCHAR(50),
-    prd_start_dt    DATE,
-    prd_end_dt      DATE,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
-);
+-- Correction on crm_prd_info table 
 
 
+
+SELECT
+    prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '.', '_') AS cat_id,
+    SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+    prd_nm,
+    ISNULL(prd_cost, 0) AS prd_cost,
+    CASE UPPER(TRIM(prd_line))
+        WHEN 'M' THEN 'Mountain'
+        WHEN 'R' THEN 'Road'
+        WHEN 'S' THEN 'Other Sales'
+        WHEN 'T' THEN 'Touring'
+        ELSE 'n/a'
+    END AS prd_line,
+    CAST(prd_start_dt AS DATE) AS prd_start_dt,
+    CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
 
 
 
@@ -69,34 +72,25 @@ INSERT INTO silver.crm_prd_info (
 			prd_start_dt,
 			prd_end_dt
 		)
-SELECT 
-prd_id,
-REPLACE(SUBSTRING(prd_key,1,5),'-','_') as Cat_id,
-SUBSTRING(prd_key,7, LEN(prd_key) ) as prd_key,
-prd_nm,
-ISNULL(prd_cost,0) as prd_cost,
-CASE 
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost,
+			CASE 
 				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
 				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
 				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
 				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
 				ELSE 'n/a'
-			END AS prd_line,
-CAST(prd_start_dt AS DATE) AS prd_start_dt,
-CAST( LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1  AS DATE) AS prd_end_dt -- Calculate end date as one day before the next start date
-from bronze.crm_prd_info
-
-
-
-
-
-
-
-
-
--- fix crm sales_details 
-
-
+			END AS prd_line, -- Map product line codes to descriptive values
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt -- Calculate end date as one day before the next start date
+		FROM bronze.crm_prd_info
 
 -- We need to change the data type of date as they are int type so before inserting the data we are going to change the data types in the table 
 
@@ -160,14 +154,13 @@ INSERT INTO silver.crm_sales_details (
 				ELSE sls_price  -- Derive price if original value is invalid
 			END AS sls_price
 		FROM bronze.crm_sales_details;
-	
-	
-
-	
 
 
 
--- insert into silver  erp_cust_az12
+
+
+
+-- Query to insert the data into silver.erp_cust_az12
 
 
 
@@ -176,52 +169,42 @@ INSERT INTO silver.erp_cust_az12 (
 			bdate,
 			gen
 		)
-SELECT 
- CASE
-  when cid LIKE 'NAS%' THEN SUBSTRING(cid,4,len(cid))
-  else CID
-  END CID,
-  CASE 
-  WHEN bdate > GETDATE()  or  bdate < '1925-10-17' then  NULL
-  else bdate
-  end as bdate,
-  CASE
-  when upper(trim(gen)) IN ('M','MALE') THEN 'MALE'
-  when upper(trim(gen)) IN ('F','FEMALE') THEN 'FEMALE'
-  ELSE 'n/a'
-  end as gen
-  FROM bronze.erp_cust_az12;
+SELECT
+			CASE
+				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) -- Remove 'NAS' prefix if present
+				ELSE cid
+			END AS cid, 
+			CASE
+				WHEN bdate > GETDATE() THEN NULL
+				ELSE bdate
+			END AS bdate, -- Set future birthdates to NULL
+			CASE
+				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+				ELSE 'n/a'
+			END AS gen -- Normalize gender values and handle unknown cases
+		FROM bronze.erp_cust_az12;
 
 
 
-
-
-
-
--- INSERT into silver.erp_loc_a101;
-
-
-
+-- Query to insert the data into silver.erp_loc_a101
 
 
 INSERT INTO silver.erp_loc_a101 (
 			cid,
 			cntry
 		)
-SELECT 
-REPLACE(cid,'-','') as cid,
-CASE 
- WHEN TRIM(cntry) = 'DE' then 'Germany'
- WHEN TRIM(cntry) IN ('US','USA') then 'United States'
- when trim(cntry) = '' or cntry IS NULL  then 'n/a'
- else trim(cntry)
- end as cntry
-FROM bronze.erp_loc_a101;
+		SELECT
+			REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
 
-
-
-
--- fixing silver.erp_px_cat_g1v2 
+--  Query to insert the data into silver.erp_px_cat_g1v2
 
 
 
